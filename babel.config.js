0d8c312dbace59a687bcd616e5b91e76c7738b8b
yungblud <killi8n@gmail.com>
2026-01-28 11:07:15 +0900

    [fix] iOS build issue

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const isTestEnv = process.env.NODE_ENV === 'test';
-
   return {
-    presets: ['babel-preset-expo'],
-    plugins: isTestEnv ? [] : ['nativewind/babel'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
