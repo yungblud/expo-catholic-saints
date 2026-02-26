27871a2f8ca26e571a64d0c0eb8ed7cb5190e78e
yungblud <killi8n@gmail.com>
2026-02-26 21:02:21 +0900

    fix: fixed expo sqlite web start

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,16 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// https://docs.expo.dev/versions/latest/sdk/sqlite/#web-setup
+// Add wasm asset support
+config.resolver.assetExts.push('wasm');
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    middleware(req, res, next);
+  };
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
